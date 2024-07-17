83c8ce9b5213bd0686344df67e0f1a480224f6e9
jala88 <cbarquero70286@ufide.ac.cr>
2024-07-16 21:01:33 -0600

    Registrarse
    Se agrega logica al controlador Home en Registrarse

--- BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
+++ BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045e1de8d4dab85d25dc5614790ced83d552f86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575a40f2bff9aa7e400470c910063278cafe1674")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
