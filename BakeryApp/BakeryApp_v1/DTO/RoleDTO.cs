b4873d504568a6c23fb9c137c43df23f64577c14
GabrielChaconF <gchacon80046@ufide.ac.cr>
2024-07-17 17:17:11 -0600

    Se arregla .gitignore

--- BakeryApp/BakeryApp_v1/DTO/RoleDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BakeryApp_v1.DTO;
-
-public class RoleDTO
-{
-    public int IdRol { get; set; }
-
-    public string NombreRol { get; set; } = null!;
-}
--- BakeryApp/BakeryApp_v1/Services/RolService.cs
+++
@@ -1,8 +0,0 @@
-﻿using BakeryApp_v1.Models;
-
-namespace BakeryApp_v1.Services;
-
-public interface RolService
-{
-    public Task<IEnumerable<Role>> ObtenerTodosLosRoles();
-}
