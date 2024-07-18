ab70256359df4784768b50192728cc055eac7242
GabrielChaconF <gchacon80046@ufide.ac.cr>
2024-07-17 18:11:58 -0600

    Se agrega layout

--- BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
+++ BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0beaf8c487e0c125547fe88fdddef3b4c375640")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934bdf68d97e3ac2142b06ad22b4b438a9627bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
