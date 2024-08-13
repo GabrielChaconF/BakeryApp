f7993c336440507932c78cc219212dbe649259d3
jala88 <cbarquero70286@ufide.ac.cr>
2024-08-13 09:48:06 -0600

    Módulo de Marketing
    Se crea la logica de marketing para usuarios registrados

--- BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
+++ BakeryApp/BakeryApp_v1/obj/Debug/net8.0/BakeryApp_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e661c9e911951c98d152cf806e7668fa8d8f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc47c28963e83e92f797d3bee0baacbfaa2a8a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryApp_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
